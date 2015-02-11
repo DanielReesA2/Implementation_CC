6ede06a8cb58d3116c22675ff8e80339ca1f83cb
Daniel <DanielReesMusic@yahoo.co.uk>
2015-02-11 15:15:53 +0000

    Working on GU, added list view which displays all ongoing projects by pulling from the projects table in taskmanagerdatabase

--- Manage_Database_GUI.py
+++ Manage_Database_GUI.py
@@ -11,10 +11,6 @@ class TaskManagerMainGUI(QMainWindow):
         super().__init__()
         self.setWindowTitle("Task Manager")
 
-        #database file related attributes
-        self.database_opened = False
-        self.database_path = None
-
         #create stacked layout
         self.stacked_layout = QStackedLayout()
         self.stacked_widget = QWidget()
@@ -30,29 +26,22 @@ class TaskManagerMainGUI(QMainWindow):
         self.setCentralWidget(self.stacked_widget)
 
         #create actions
-        self.open_database = QAction("Open Database",self)
-        self.close_database = QAction("Close Database",self)
         self.insert_data = QAction("Insert Data",self)
         self.update_data = QAction("Update Data",self)
         self.delete_data = QAction("Delete Data",self)
 
         #create bars
         self.main_tool_bar_1 = QToolBar()
-        self.main_tool_bar_2 = QToolBar()
         self.main_menu_bar = QMenuBar()
         self.main_status_bar = QStatusBar()
 
         #add actions to toolbar
-        self.main_tool_bar_2.addAction(self.open_database)
-        self.main_tool_bar_2.addAction(self.close_database)
         self.main_tool_bar_1.addAction(self.insert_data)
         self.main_tool_bar_1.addAction(self.update_data)
         self.main_tool_bar_1.addAction(self.delete_data)
 
         #add actions to menubars
         self.database_menu = self.main_menu_bar.addMenu("Database")
-        self.database_menu.addAction(self.open_database)
-        self.database_menu.addAction(self.close_database)
         self.database_menu.addAction(self.insert_data)
         self.database_menu.addAction(self.update_data)
         self.database_menu.addAction(self.delete_data)
@@ -62,38 +51,16 @@ class TaskManagerMainGUI(QMainWindow):
 
         #add bars to mainwindow
         self.addToolBar(self.main_tool_bar_1)
-        self.addToolBar(self.main_tool_bar_2)
         self.setMenuBar(self.main_menu_bar)
         self.setStatusBar(self.main_status_bar)
 
         #set window size/position
         self.resize(700,500)
         self.move(100,100)
-
-        #connections
-        self.open_database.triggered.connect(self._open_database)
-        self.close_database.triggered.connect(self._close_database)
-
-    def _open_database(self):
-        file_name = QFileDialog.getOpenFileName()
-        print("{0} Opened".format(file_name))
-        self.main_status_bar.showMessage("{0} Opened".format(file_name))
-        self.database_opened = True
-        self.database_path = file_name
-        self.update_main_table()
-
-    def _close_database(self):
-        print("{0} Closed".format(self.database_path))
-        self.main_status_bar.showMessage("{0} Closed".format(self.database_path))
-        time.sleep(0.5)
-        self.main_status_bar.showMessage("Task Manager ver1.0")
-        self.database_opened = False
-        self.database_path = None
-        self.update_main_table()
         
     def CreateMainWindow(self):
         #components
-        self.main_tree_view = QTreeView()
+        self.main_list_view = QListView()
         self.main_table_view = QTableView()
         self.label1 = QLabel("""Details:""")
         self.test = QPushButton("""TEST""")                
@@ -112,7 +79,7 @@ class TaskManagerMainGUI(QMainWindow):
         #add details layout to details widget
         self.details_widget.setLayout(self.details_layout)
         #add components to layout
-        self.main_layout.addWidget(self.main_tree_view,0,0)
+        self.main_layout.addWidget(self.main_list_view,0,0)
         self.main_layout.addWidget(self.main_table_view,0,1)
         self.main_layout.addWidget(self.details_widget,0,2)
         #create widget to hold layout
@@ -120,49 +87,51 @@ class TaskManagerMainGUI(QMainWindow):
         #add layout to widget
         self.main_widget.setLayout(self.main_layout)
 
+        self.update_components()
+
+    def update_components(self):
+        self.update_main_list()
         self.update_main_table()
+        
+    def update_main_list(self):
+        results = select_all_from_project()
+        projects = []
+        for item in results:
+            projects.append(item[1])
+        list_model = main_list_view_model(projects)
+        self.main_list_view.setModel(list_model)
 
     def update_main_table(self):
-        if self.database_opened:
-            print("Database Opened")
-            #display all tasks as default
-            results = select_all_from_task()
-            
-                        
+        #display all tasks as default
+        results = select_all_from_task()
 
-##            with sqlite3.connect("Task_Manager_Database.db") as db:
-##                cursor = db.cursor()
-##                cursor.execute("pragma table_info(Task)")
-##                results = cursor.fetchall()
-##                
-##            horHeaders = []
-##            for item in results:
-##                horHeaders.append(item[1])
-##            self.main_table_widget.setHorizontalHeaderLabels(horHeaders)
-##        
-##            with sqlite3.connect(self.database_path) as db:
-##                cursor = db.cursor()
-##                cursor.execute("""select a.TaskID,a.TaskName,a.DueDate,a.Priority,b.TechnicalAreaName,d.ProjectName,c.TaskManagerName
-##from Task a
-##join TechnicalArea b 
-##on a.TechnicalAreaID = b.TechnicalAreaID
-##join TaskManager c
-##on a.TaskManagerID = c.TaskManagerID
-##join Project d
-##on a.ProjectID = d.ProjectID""")
-##                data = cursor.fetchall()
-##
-##                for item in data:
-##                    row = 0
-##                    for item2 in item:
-##                        column = 0
-##                        new_item = QTableWidgetItem(item2)
-##                        self.main_table_widget.setItem(column,row,new_item)
-##                        column = column + 1
-##                    row = row + 1
-                        
+class main_table_view_model(QAbstractTableModel):
+    def __init__(self):
+        super().__init__()
 
+class main_list_view_model(QAbstractListModel):
+    def __init__(self,projects):
+        super().__init__()
+        self._Projects = projects
+
+    def rowCount(self,parent):
+        return len(self._Projects)
+
+    def data(self,index,role):
+        if role == Qt.DisplayRole:
+            row = index.row()
+            value = self._Projects[row]
+            return value
         
+##    def flags(self,index):
+##        return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
+##
+##    def setData(self,index,value,role = Qt.EditRole):
+##        if role == Qt.EditRole:
+##            row = index.row()
+##            
+##            update_one_for_Table(row,"Project")
+##            self.dataChanged.emit(index,index)
         
 if __name__ == "__main__":
     application = QApplication(sys.argv)
